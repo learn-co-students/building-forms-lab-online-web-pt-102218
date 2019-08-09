0df43e330daf1f68e37bda9ca6dc31a74eff7aba
Matthew McCormack <mmccormack1182@gmail.com>
2019-08-09 12:05:20 -0400

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,47 @@
 // Add BandInput component
 import React, { Component } from 'react'
 
+
 class BandInput extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: ''
+    }
+  }
+
+  handleOnChange = event => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault()
+    this.props.addBand(this.state)
+    this.setState({
+      name: ''
+    })
+  }
+
+
   render() {
     return(
       <div>
-        Band Input
+        <form onSubmit={this.handleOnSubmit}>
+          <label>Band Name</label>
+          <input onChange={this.handleOnChange}/>
+          <button type="submit" >Add Band</button>
+        </form>
       </div>
     )
   }
 }
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//       addBand: () => dispatch(this.props.addBand())
+//     }
+// }
 
 export default BandInput
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react'
+import BandInput from '../components/BandInput'
+import { connect } from 'react-redux'
+
 
 class BandsContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      bands: []
+    }
+  }
+
+
   render() {
     return(
       <div>
-        BandsContainer
+        <BandInput addBand={this.props.addBand}/>
+        <ul>
+        {this.props.bands.map((band) => <li>{band.name}</li>)}
+        </ul>
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapStateToProps = ({bands}) => ({bands})
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addBand: (band) => dispatch({type: 'ADD_BAND', band: band })
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer)
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     // dispatching actions returned by action creators
+//     increment: () => dispatch(increment()),
+//     decrement: () => dispatch(decrement()),
+//     reset: () => dispatch(reset())
+//   }
+
+// const increment = () => ({ type: 'INCREMENT' })
